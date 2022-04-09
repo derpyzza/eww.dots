85746e6ce5abf519da8c144ba6fc3882f6426e45
derpyzza <derpyzza@gmail.com>
2022-04-09 20:10:16 +0500

    Adds most of the necessary features

--- scripts/work.py
+++ scripts/work.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python
 #by github.com/derpyzza :)
-
 from subprocess import run
-from sys import argv as args
-import difflib
-
-# comment
 # (defwidget workspaces [] (literal :content workspaces))
 #                          │
 #                          │
@@ -29,17 +24,26 @@ def run_get(input):
     return run(input, capture_output=True, text=True, shell=True).stdout.replace('\n', ' ').strip()
 
 workspaces = run_get("bspc query -D --names").split()
+# Unnecessary. removes extra desktop on my laptop's screen ( i use an external monitor for most of my daily usage )
 if workspaces[6] == "Desktop":
     workspaces.remove("Desktop")
 focused = run_get("bspc query -D -d focused --names")
 occupied = run_get("bspc query -D -d .occupied --names").split()
-final = "(box :orientation \"v\" "
+# Final literal widget string to be outputed.
+final = "(box :spacing -5 :orientation \"v\" "
 
 for work in workspaces:
+    wk = ''
+    ic=''
     if work == focused:
-        final += "(work :desk \"" + work + "\" :class \"focused\") "
+        wk = 'focused'
     else:
-        final += "(work :desk \"" + work + "\" :class \"unoccupied\") "
+        wk = 'unoccupied'
+    for o in occupied:
+        if work == o and work != focused:
+            wk = 'occupied'
+            ic=''
+    final += "(work :desk \"" + work + "\" :class \""+wk+"\" :icon \""+ic+"\" ) "
 final += ")"
 
 print(final)
